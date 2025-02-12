5b5639a4fd01f94bbf4c2dfb1ea1b1f27278d97b
Jacob Kampf <jkampf@wisc.edu>
2025-02-12 13:17:39 -0600

    Completed Activity 6 Lessons 1 & 2 again with proper data

--- boilerplate/js/main.js
+++ boilerplate/js/main.js
@@ -2,7 +2,7 @@
 
 //Create the map
 function createMap() {
-    var map = L.map('map').setView([44.41154690517817, -89.63139921756608], 8);
+    var map = L.map('map').setView([46.00318583226062, -94.60267026275974], 7);
 
     var OpenStreetMap_Mapnik = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	    maxZoom: 19,
@@ -13,40 +13,43 @@ function createMap() {
     getData(map)
 }
 
-
+//Calculate minimum values for proportional circles
 function calculatedMinValue(data) {
     //Create an empty array to store all data values
-    var allValues = [];
-    //Loop through each feature
-    for(var i = 0; i < data.features.length; i++) {
-        //Get current feature
-        var feature = data.features[i];
-        //Get longitude value of current feature
-        var longitude = feature.geometry.coordinates[0];
-        //Add value to array
-        allValues.push(longitude);
-    }
-    //Get minimum value for array
-    var minValue = Math.min(...allValues);
+   var allValues = [];
 
-    return minValue;
+   //Loop through each city
+   for(var i=0; i < data.features.length; i++) {
+    var properties = data.features[i].properties;
+    //Loop through each year
+    for(var year = 2010; year <= 2019; year ++) {
+        //Get population for current year
+        var value = properties[String(year) + " Pop" ]; 
+        //Add values to array
+        allValues.push(value);
+    }
+   }
+   //Get minimum value in array
+   var minValue = Math.min(...allValues);
+
+   return minValue;
 }
 
 //Calculate radius of each proportional symbol
 function calcPropRadius(attValue) {
     //Constant factor adjusts symbol sizes evenly
-    var minRadius = 15;
+    var minRadius = 3;
+    console.log(minValue);
     //Flannery Appearance Compensation formula.  Changed the multiplyer to really show the changes in longitude, otherwise it was unnoticable
-    var radius = 1.0083 * Math.pow(attValue/minValue,15.5715) * minRadius;
+    var radius = 1.0083 * Math.pow(attValue/minValue,0.5715) * minRadius;
 
     return radius;
 }
 
 function pointToLayer(feature, latlng){
-    if (!feature.geometry || !feature.geometry.coordinates) {
-        console.error("Invalid feature geometry:", feature);
-        return null;
-    }
+
+    //Attribute to symbolize
+    var attribute = "2010 Pop";
 
     // Create marker options
     var geojsonMarkerOptions = {
@@ -57,35 +60,26 @@ function pointToLayer(feature, latlng){
         fillOpacity: 0.8
     };
 
-    // Extract longitude safely
-    var longValue = Number(feature.geometry.coordinates[0]);
-
-    if (isNaN(longValue)) {
-        console.warn("Invalid longitude value:", feature.geometry.coordinates);
-        longValue = 1; // Default fallback value
-    }
+    //For each feature, determine its value for the selected attribute
+    var attValue = Number(feature.properties[attribute]);
 
     // Set the marker radius based on longitude
-    geojsonMarkerOptions.radius = calcPropRadius(longValue);
+    geojsonMarkerOptions.radius = calcPropRadius(attValue);
 
     // Create circle marker layer
     var layer = L.circleMarker(latlng, geojsonMarkerOptions);
 
     // Popup content
     var popupContent = `
-        <p><b>Facility Name: </b>${feature.properties.FAC_NM}<br>
-        <b>Facility ID: </b>${feature.properties.LOC_ID}<br>
-        <b>Facility Type: </b>${feature.properties.FAC_TY}<br>
-        <b>Facility Ownership: </b>${feature.properties.FAC_OWSP}<br>
-        <b>Facility Use: </b>${feature.properties.FAC_USE}<br>
-        <b>Transmission Region: </b>${feature.properties.TRANS_RGN}<br>
-        <b>More information: </b><a href="${feature.properties.F5010URL}" target="_blank">Link</a></p>
+        <p><b>County: </b>${feature.properties.CTY_NAME}<br>
+        <b>2010 Population: </b>${feature.properties[attribute]}<br>
+        </p>
     `;
 
     // Bind popup to circle marker
     layer.bindPopup(popupContent);
 
-    return layer; // ✅ Ensure we return a valid layer
+    return layer; // 
 }
 
 // Function to create symbols
@@ -97,10 +91,22 @@ function createPropSymbols(data, map){
     }).addTo(map);
 }
 
+//Create new sequence controls
+function createSequenceControls() {
+    var slider = "<input class='range-slider' type='range'></input>";
+    document.querySelector("#panel").insertAdjacentHTML('beforeend',slider);
+
+    //set slider attributes
+    document.querySelector(".range-slider").max = 6;
+    document.querySelector(".range-slider").min = 0;
+    document.querySelector(".range-slider").value = 0;
+    document.querySelector(".range-slider").step = 1;
+}
+
 //Step 2: Import GeoJSON data
 function getData(map){
     //load the data
-    fetch('data/WiscAirports.geojson')
+    fetch('data/MN_County_Pop.geojson')
         .then(function(response){
             return response.json();
         })
@@ -109,6 +115,8 @@ function getData(map){
             minValue = calculatedMinValue(json);
             //call function to create proportional symbols
             createPropSymbols(json, map);
+            //Function for sequence control
+            createSequenceControls();
         })
 };
 
